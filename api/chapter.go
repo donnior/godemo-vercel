d32f31f95b896e1e8074a32be68390ceee20ed98
Xie Dongming <xiedongming@orientalmind.com>
2024-03-05 14:53:26 +0800

    add test url

--- api/chapter.go
+++ api/chapter.go
@@ -17,9 +17,9 @@ import (
 
 func ChapterHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	res, err := ListChapter("some book link", SiteConfig{
-		Domain:          "some domain",
-		ChapterSelector: "some selector",
+	res, err := ListChapter("https://www.sumingxs.com/xiaoshuo/2/", SiteConfig{
+		Domain:          "www.sumingxs.com",
+		ChapterSelector: "#list li",
 		ContentSelector: "some selector",
 	})
 	if err != nil {
